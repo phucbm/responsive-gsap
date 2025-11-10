b0e397914a5c8c9727af713075dd801a2d2fc31e
phucbm <hiphucbui@gmail.com>
2025-11-10 16:27:37 +0700

    feat(test): remove happy dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'happy-dom',
+    environment: 'jsdom',
     globals: true,
     setupFiles: ['./tests/setup.ts'],
     coverage: {
